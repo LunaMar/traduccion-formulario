9347cca6caf360a995d79dbc9943a552c001f805
LunaMar <letera.lmc@gmail.com>
2016-10-24 20:45:19 -0300

    traduccion formulario

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Ejercicio1//
+var signIn = document.getElementById('form-signin-heading');
+signIn.innerHTML = "Por favor inicia sesion";
+
+//Ejercicio2//
+
+var correo = document.getElementById ('inputEmail');
+ correo.placeholder = "Correo Electronico";
+
+ //Ejercicio3//
+
+ var contraseña = document.getElementById('inputPassword');
+ contraseña.placeholder ="Contraseña";
+
+ //Ejercicio4//
+
+ var recordarDatos = document.getElementsByTagName("span")[0];
+ recordarDatos.innerHTML ="Recordar Datos";
+
+ //Ejercicio5//
+ var iniciarSesion =document.getElementsByClassName ('btn')[0];
+ iniciarSesion.innerHTML = "Iniciar Sesion";
+
